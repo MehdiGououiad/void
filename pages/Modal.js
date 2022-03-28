f97e6e2bc10db524a0c83d3ca8a8beee72f8a592
mehdigououiad <mehdigououiad@gmail.com>
2022-03-28 06:26:01 +0000

    staticsiterendering

--- pages/Modal.js
+++ pages/Modal.js
@@ -1,17 +1,16 @@
 
 
-export default function StorieModal(props) {
+export default function Modal({url,hideSingleStorie}) {
   
-
+console.log(url)
   return (
-    <div onClick={props.hideSingleStorie} className='fixed w-full h-full top-0 left-0  flex   z-10'>
-    <div  className='w-full h-full bg-gray-700 opacity-90  absolute'>
+    <div onClick={hideSingleStorie} className='fixed w-full h-full top-0 left-0  flex   z-10'>
+    {/* <div  className='w-full h-full bg-gray-700 opacity-90  absolute'>
 
     </div>
-    <div className="z-10 mt-24 w-full">
-<h1 className="text-2xl text-center">Prenez rendez-vous avec le Dr Laurianne Giraudeau
-</h1>
-    </div>
+    <div className="z-10 mt-24 w-full"> */}
+<iframe className="w-3/5 ml-[20%]" src={url} frameBorder="0"></iframe>
+    {/* </div> */}
 
 </div>
     
